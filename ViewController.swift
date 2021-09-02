fdb4af316816258a758f273385a41b3aa6efd9c0
Student04 <student04@TFNS5304.techni.dom>
2021-09-02 16:28:46 +0200

    debut recap

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-Reseau-Appel
+//
+//  Created by Student04 on 02/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
