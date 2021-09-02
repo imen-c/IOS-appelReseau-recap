308f3bb8df2275c60d65a5795268f751e5be4b69
Student04 <student04@TFNS5304.techni.dom>
2021-09-02 16:55:32 +0200

    init

--- ViewController.swift
+++ ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var banner: UIImageView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
